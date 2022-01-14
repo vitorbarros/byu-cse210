ec110071993910a2405300d3c1cd12c364a270bb
Vitor Monteiro de Barros <vitormonteirodebarros@gmail.com>
2022-01-13 22:54:17 -0300

    w02

---
+++ w02/abstraction_teach_one_another.py
@@ -0,0 +1,28 @@
+class Player:
+    """this class is responsible for controlling the amount of points that the player has and the guesses"""
+
+    def __init__(self):
+        self.points = 300
+
+    def guess(self):
+        """get the user input"""
+        return
+
+    def points_increase(self):
+        """this method is responsible by increasing the points if the guess is correct"""
+        return
+
+    def points_decrease(self):
+        """this method is responsible by decreasing the points if the guess is incorrect"""
+        return
+
+
+class Card:
+    """this class is responsible for controlling the cards that should be displayed"""
+
+    def __init__(self):
+        self.cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+    def display_card(self):
+        """this method is responsible for displaying a card randomly"""
+        return
